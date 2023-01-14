38ba311cefd9827b41e379a617cb7f5a99dcf621
franco@mumuki.org <franco@mumuki.org>
2023-01-14 16:17:11 +0000

    Mumuki Export on 2023-01-14 16:17:11 +0000

--- 00003_Novo nome, nova vida/test.py
+++ 00003_Novo nome, nova vida/test.py
@@ -1,12 +1,12 @@
 class Test(unittest.TestCase):
   def test_loc_code_foi_renomeada(self):
-    self.assertFalse("loc_code" in cines, "a coluna loc_code não deve mais existir")
-    self.assertTrue("city_id" in cines, "column city_id deve existir agora")
+    self.assertFalse("loc_code" in cinemas, "a coluna loc_code não deve mais existir")
+    self.assertTrue("city_id" in cinemas, "column city_id deve existir agora")
 
   def test_prov_id_foi_renomeada(self):
-    self.assertFalse("prov_id" in cines, "coluna prov_id não deve mais existir")
-    self.assertTrue("province_id" in cines, "a coluna província_id deve existir agora")
+    self.assertFalse("prov_id" in cinemas, "coluna prov_id não deve mais existir")
+    self.assertTrue("province_id" in cinemas, "a coluna província_id deve existir agora")
   
   def test_dep_id_foi_renomeada(self):
-    self.assertFalse("dep_id" in cines, "coluna dep_id não deve mais existir")
+    self.assertFalse("dep_id" in cinemas, "coluna dep_id não deve mais existir")
     self.assertTrue("department_id" in cinemas, "a coluna department_id deve existir agora")
