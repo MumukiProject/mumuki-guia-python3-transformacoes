04b993651d8b0b63eb97404ba51db2ea31661a27
franco@mumuki.org <franco@mumuki.org>
2023-01-14 16:12:26 +0000

    Mumuki Export on 2023-01-14 16:12:26 +0000

--- 00002_Oi e tchau/test.py
+++ 00002_Oi e tchau/test.py
@@ -1,10 +1,10 @@
 class Test(unittest.TestCase):
   
   def test_obs_foi_removida(self):
-     self.assertFalse("obs" in cines, "coluna obs deveria ter sido removida")
+     self.assertFalse("obs" in cinemas, "coluna obs deveria ter sido removida")
   
   def test_category_foi_removida(self):
-     self.assertFalse("category" in cines, "a coluna de categoria deveria ter sido removida")
+     self.assertFalse("category" in cinemas, "a coluna de categoria deveria ter sido removida")
     
   def test_update_year_foi_removida(self):
-     self.assertFalse("update_year" in cines, "a coluna update_year deveria ter sido removida")
+     self.assertFalse("update_year" in cinemas, "a coluna update_year deveria ter sido removida")
