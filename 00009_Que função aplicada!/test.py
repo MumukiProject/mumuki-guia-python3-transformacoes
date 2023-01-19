072fce6eaf05ce2309cd410cf298a2f4735e55e1
franco@mumuki.org <franco@mumuki.org>
2023-01-19 16:23:02 +0000

    Mumuki Export on 2023-01-19 16:23:01 +0000

--- 00009_Que função aplicada!/test.py
+++ 00009_Que função aplicada!/test.py
@@ -4,11 +4,11 @@ class Test(unittest.TestCase):
     self.assertTrue("estimated_foreign_income" in cruzeiristas)
     
   def test_cruceristas_de_uruguay_tienen_ingreso_estimado_cero(self):
-    self.assertEquals(cruzeiristas.set_index("cruise_id").loc[5823, "estimated_foreign_income"], 0, "crucerista 5823 debería tener estimated_foreign_income en 0")    
+    self.assertEquals(cruzeiristas.set_index("cruise_id").loc[5823, "estimated_foreign_income"], 0, "crucerista 5823 deveria ter estimated_foreign_income em 0")    
     
   def test_cruceristas_de_brasil_tienen_ingreso_estimado_no_cero(self):
-    self.assertEquals(cruzeiristas.set_index("cruise_id").loc[7404, "estimated_foreign_income"], 224.76, "crucerista 7404 debería tener estimated_foreign_income en 224.76")
+    self.assertEquals(cruzeiristas.set_index("cruise_id").loc[7404, "estimated_foreign_income"], 224.76, "crucerista 7404 deveria ter estimated_foreign_income em 224.76")
     
   def test_cruceristas_de_otro_pais_de_asia_tienen_ingreso_estimado_no_cero(self):
-    self.assertEquals(int(cruzeiristas.set_index("cruise_id").loc[7638, "estimated_foreign_income"]), 125, "crucerista 7638 debería tener estimated_foreign_income en 125 aproximadamente")
+    self.assertEquals(int(cruzeiristas.set_index("cruise_id").loc[7638, "estimated_foreign_income"]), 125, "crucerista 7638 deveria ter estimated_foreign_income em 125 aproximadamente")
     
