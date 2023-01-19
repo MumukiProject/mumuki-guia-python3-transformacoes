0fd9e6d4197daa14fc72441e5d60f5383af51acf
franco@mumuki.org <franco@mumuki.org>
2023-01-19 03:58:08 +0000

    Mumuki Export on 2023-01-19 03:58:08 +0000

--- 00005_O mapa do cruzeiro/test.py
+++ 00005_O mapa do cruzeiro/test.py
@@ -21,7 +21,7 @@ class Test(unittest.TestCase):
   def test_cruzeiros_do_uruguai_são_nacionais(self):
     indexado = cruises.set_index("cruise_id")
     
-    self.assertEquals(indexed.loc[5823, "region"], "Nacional")
+    self.assertEquals(indexed.loc[5823, "region"], "National")
     
   def test_cruzeiros_do_uea_são_nan(self):
     indexado = cruises.set_index("cruise_id")
