6e6fb878196ef38a8a55b54326537dcd5b7431c2
franco@mumuki.org <franco@mumuki.org>
2023-01-19 04:06:42 +0000

    Mumuki Export on 2023-01-19 04:06:42 +0000

--- 00005_O mapa do cruzeiro/test.py
+++ 00005_O mapa do cruzeiro/test.py
@@ -4,7 +4,7 @@ class Test(unittest.TestCase):
     self.assertTrue("region" in cruzeiros.columns)
 
   def test_cruzeiros_do_brasil_são_regionais(self):
-    indexado = cruises.set_index("cruise_id")
+    indexado = cruzeiros.set_index("cruise_id")
     
     self.assertEquals(indexed.loc[10404, "region"], "Regional")
     self.assertEquals(indexed.loc[1494, "region"], "Regional")
@@ -12,18 +12,18 @@ class Test(unittest.TestCase):
     
 
   def test_cruzeiros_da_argentina_são_regionais(self):
-    indexado = cruises.set_index("cruise_id")
+    indexado = cruzeiros.set_index("cruise_id")
     
     self.assertEquals(indexed.loc[5296, "region"], "Regional")
     self.assertEquals(indexed.loc[2537, "region"], "Regional")
     self.assertEquals(indexed.loc[7237, "region"], "Regional")
     
   def test_cruzeiros_do_uruguai_são_nacionais(self):
-    indexado = cruises.set_index("cruise_id")
+    indexado = cruzeiros.set_index("cruise_id")
     
     self.assertEquals(indexed.loc[5823, "region"], "Nacional")
     
   def test_cruzeiros_do_uea_são_nan(self):
-    indexado = cruises.set_index("cruise_id")
+    indexado = cruzeiros.set_index("cruise_id")
     
     self.assertTrue(pd.isna(indexed.loc[8829, "region"]))
